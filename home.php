2e6da1306121adc736ba6d8e050f5f654c01f29a
Renuka-M-Anjutagi <renuka.c2@gmail.com>
2024-03-18 20:23:19 +0530

    netlify-new

---
+++ home.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><Html:5>
+
+
+
+    </Html:5></title>
+</head>
+<body>
+    <Form>
+        <input type="text" name="" id="">
+
+    </Form>
+</body>
+</html>
